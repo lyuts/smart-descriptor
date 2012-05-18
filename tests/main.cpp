b2d9c817deea739a1a85405cb04264ce91182073
lyuts <dioxinu@gmail.com>
2012-05-18 23:35:36 +0300

    Added unittests main and cmake file for compilation.

---
+++ tests/main.cpp
@@ -0,0 +1,43 @@
+#include <cppunit/extensions/TestFactoryRegistry.h>
+#include <cppunit/ui/text/TestRunner.h>
+#include <cppunit/TestResult.h>
+#include <cppunit/TestResultCollector.h>
+#include <cppunit/CompilerOutputter.h>
+
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    CppUnit::TestRunner runner;
+
+    CppUnit::TestFactoryRegistry &registry = CppUnit::TestFactoryRegistry::getRegistry();
+
+    runner.addTest(registry.makeTest());
+
+    CppUnit::TestResult controller;
+    CppUnit::TestResultCollector result;
+    controller.addListener(&result);
+
+    runner.run(controller);
+
+    CppUnit::CompilerOutputter outputter(&result, std::cerr);
+    outputter.write();
+
+    /** STATS **/
+    int numErrors = result.testErrors();
+    int numFailures = result.testFailures();
+    int numFailuresTotal = result.testFailuresTotal();
+    int numTests = result.runTests();
+
+    std::cout << "=============================================" << std::endl;
+    std::cout << " STATISTICS" << std::endl;
+    std::cout << "=============================================" << std::endl;
+    std::cout << "Number of errors (uncaught exception):  " << numErrors << std::endl;
+    std::cout << "Number of failures (failed assertions): " << numFailures << std::endl;
+    std::cout << "Total number of detected failures:      " << numFailuresTotal << std::endl;
+    std::cout << "Total number of tests:                  " << numTests << std::endl;
+    std::cout << "=============================================" << std::endl;
+
+    return result.wasSuccessful() ? 0 : 1;
+}
+
